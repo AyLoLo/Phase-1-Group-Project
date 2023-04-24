260d1c170dadef7b85dc2005132814b44fc9eb96
MLBMokey <128417036+MLBMokey@users.noreply.github.com>
2023-04-24 12:38:28 -0400

    Add files via upload

--- Phase-1-Group-Project/Game Of Love.js
+++ Phase-1-Group-Project/Game Of Love.js
@@ -1,7 +1,20 @@
-fetch("https://thronesapi.com/api/v2/Characters")
+const characterTitle = document.getElementById('title')
+const characterFullName = document.getElementById('full-name')
+const characterDescription = document.getElementById('description')
+const characterImage = document.getElementById('detail-image')
+const nav = document.getElementById('character-list')
+
+function navDisplay(character) {
+    navImages = document.createElement('img')
+    navImages.src = character.imageUrl
+    nav.appendChild(navImages)
+}
+
+fetch(" http://localhost:3000/Characters")
     .then(response => response.json())
     .then(characters => {
         characters.forEach(character=> {
-            
+            navDisplay(character)
         });
     })
+
